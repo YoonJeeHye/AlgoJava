a6967c00e07eb95e48cc8790bcb7df228c551e9c
nowgnas <marshmello.sw@gmail.com>
2022-08-05 00:48:12 +0900

    problem solve: 올바른 괄호

--- src/week1_이상원/Test.java
+++
@@ -1,7 +0,0 @@
-package week1_이상원;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
---
+++ src/week1_이상원/올바른괄호.java
@@ -0,0 +1,24 @@
+package week1_이상원;
+
+import java.util.Objects;
+import java.util.Stack;
+
+public class 올바른괄호 {
+    public static void main(String[] args) {
+        String s = "(())()";
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.isEmpty()) {
+                stack.push(String.valueOf(s.charAt(i)));
+            } else {
+                if (Objects.equals(stack.peek(), "(") && s.charAt(i) == ')') {
+                    stack.pop();
+                } else {
+                    stack.push(String.valueOf(s.charAt(i)));
+                }
+            }
+        }
+//        return stack.isEmpty();
+    }
+}

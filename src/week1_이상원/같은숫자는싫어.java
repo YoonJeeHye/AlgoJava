cbd37e21839c92ac5a98e68b624cdd8ae6c2cae3
nowgnas <marshmello.sw@gmail.com>
2022-08-05 08:24:03 +0900

    prob solve: 올바른 괄호

---
+++ src/week1_이상원/같은숫자는싫어.java
@@ -0,0 +1,20 @@
+package week1_이상원;
+
+import java.util.Stack;
+
+public class 같은숫자는싫어 {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 3, 3, 0, 1, 1};
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(arr[i]);
+            }
+            if (stack.peek() != arr[i]) {
+                stack.push(arr[i]);
+            }
+        }
+        Object[] answer = stack.toArray();
+//        return answer;
+    }
+}

3b02238bc38cdf595540de24e3052d247030c168
김영경 <54929520+hubhubhub98@users.noreply.github.com>
2022-08-21 19:32:13 +0900

    2022-08-21 완료

---
+++ src/week3_김영경/게임맵최단거리.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] maps) {
+        Queue<int[]> q=new ArrayDeque<>();
+        q.add(new int[]{0,0,1});
+        int cnt=0;
+        int x=maps.length-1;
+        int y=maps[0].length-1;
+        while(!q.isEmpty()){
+            int[] tmp=q.poll();
+            if(tmp[0]==-1 || tmp[1]==-1 ||tmp[0]==x+1 || tmp[1]==y+1 || maps[tmp[0]][tmp[1]]<=0)
+                continue;
+            if(tmp[0]==x && tmp[1]==y)
+                return tmp[2];
+            
+            maps[tmp[0]][tmp[1]]=-1;
+            //System.out.println(tmp[0]+" "+tmp[1]);
+            q.add(new int[]{tmp[0]-1,tmp[1],tmp[2]+1});
+            q.add(new int[]{tmp[0]+1,tmp[1],tmp[2]+1});
+            q.add(new int[]{tmp[0],tmp[1]-1,tmp[2]+1});
+            q.add(new int[]{tmp[0],tmp[1]+1,tmp[2]+1}); 
+            }
+        return -1;
+    }
+}

71cf981ebcd8d889565c6d86c8270fc93590894d
Dodamon <dodamond222@gmail.com>
2022-09-04 02:50:01 +0900

    [2022-09-04 이예은] A021_BJ2156_포도주시식

--- src/week5/week5_이예은/A021_BJ2156_포도주시식.java
+++ src/week5/week5_이예은/A021_BJ2156_포도주시식.java
@@ -13,11 +13,14 @@ public class A021_BJ2156_포도주시식 {
 	
 	public static int total_wine() {
 		dp[1] = wine[1];
-		if(N == 1) return wine[1];
+		if(N == 1) return wine[1]; // 와인이 한개 밖에 없다면 경우의 수가 하나 이다
 		
 		dp[2] = wine[1] + wine[2];
-		for(int i = 3; i <= N; i++) {
+		for(int i = 3; i <= N; i++) { // 와인이 2개 이상이라면 dp의 표를 채울 수 있다
+			// dp[i] = i번째 와인이 들어 있는 컵에 도착 했을때 최대로 많이 먹을 수 있는 와인의 양
+			// i번째 와인을 먹을 때
 			dp[i] = dp[i-2] < dp[i-3] + wine[i-1]? dp[i-3] + wine[i-1] + wine[i]: dp[i-2] + wine[i];
+			// i번째 와인을 먹지 않을 때
 			dp[i] = dp[i] < dp[i-1] ? dp[i-1]: dp[i];
 		}
 		return dp[N];

aca522c2d95816f79e2cbb31b3eb8db2fed2492c
Dodamon <dodamond222@gmail.com>
2022-09-03 15:14:51 +0900

    [2022-09-03 이예은] A023_BJ11047_동전0

---
+++ src/week5/week5_이예은/A023_BJ11047_동전0.java
@@ -0,0 +1,43 @@
+package week5.week5_이예은;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class A023_BJ11047_동전0 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuffer sb = new StringBuffer();
+		
+		String[] inputs = br.readLine().split(" ");
+		int N = Integer.parseInt(inputs[0]);
+		int K = Integer.parseInt(inputs[1]);
+		int[] coin = new int[N];
+	
+		
+		for(int i = 0; i < N; i++) {
+			coin[i] = Integer.parseInt(br.readLine());
+
+		}
+		
+		int total = K;
+		int count = 0;
+		for(int i = N-1; i >= 0; i--) {
+			if(total == 0) break;
+			if(total >= coin[i]) {
+				count += total/coin[i];
+				total %= coin[i];
+			}
+		}
+		/*---------------출력----------------*/
+		bw.write(count + "");
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}

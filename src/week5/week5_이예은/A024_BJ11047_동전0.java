5cebd2f4ae4930a65eaf63f9635c4ece777d732b
Dodamon <dodamond222@gmail.com>
2022-09-03 15:16:23 +0900

    [2022-09-03 이예은] A024_BJ11047_동전0

--- src/week5/week5_이예은/A024_BJ11047_동전0.java
+++ src/week5/week5_이예은/A024_BJ11047_동전0.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 
-public class A023_BJ11047_동전0 {
+public class A024_BJ11047_동전0 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

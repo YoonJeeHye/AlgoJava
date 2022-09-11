dd39e0a88a1f516d510fe5251af2309b6586db4e
nowgnas <marshmello.sw@gmail.com>
2022-09-11 23:39:19 +0900

    [22.09.11 이상원]문제 풀이

---
+++ src/week6/week6_이상원/여행경로.java
@@ -0,0 +1,35 @@
+package week6.week6_이상원;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class 여행경로 {
+    static String[][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL", "SFO"}};
+    static Queue<String> queue = new PriorityQueue<>();
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        visited = new boolean[tickets.length];
+        dfs(tickets, "ICN", 0, "ICN");
+
+        String[] result = queue.poll().split(",");
+//        return result;
+        System.out.println(Arrays.toString(result));
+    }
+
+    static void dfs(String[][] tickets, String cur, int cnt, String path) {
+        if (cnt == tickets.length) {
+            queue.add(path);
+            return;
+        }
+
+        for (int i = 0; i < tickets.length; i++) {
+            if (!visited[i] && cur.equals(tickets[i][0])) {
+                visited[i] = true;
+                dfs(tickets, tickets[i][1], cnt + 1, path + "," + tickets[i][1]);
+                visited[i] = false;
+            }
+        }
+    }
+}

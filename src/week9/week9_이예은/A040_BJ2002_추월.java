4bc62509dddd894bfb7cc8acfb35d9d39eb77803
Dodamon <dodamond222@gmail.com>
2022-10-08 20:20:02 +0900

    [2022-10-08 이예은] A040_BJ2002_추월

--- src/week9/week9_이예은/A040_BJ2002_추월.java
+++ src/week9/week9_이예은/A040_BJ2002_추월.java
@@ -14,28 +14,29 @@ class A040_BJ2002_추월 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int N = Integer.parseInt(br.readLine());
-		HashMap<String, Boolean> checked = new HashMap<>(); // 해당 번호판을 체크했는지 확인한다
-		String[] list = new String[N]; // 터널에 들어오는 순서대로 번호판을 저장한다
+		HashMap<String, Boolean> checked = new HashMap<>(); // 해당 번호판을 체크했는지 확인는 해쉬맵
+		String[] list = new String[N]; //터널에 들어오는 순서대로 번호판을 저장하는 배열
 		
 		for(int i = 0; i < N; i++) {
 			String car = br.readLine();
-			list[i] = car;
-			checked.put(car, false);
+			list[i] = car; // 터널에 들어오는 순서대로 번호판을 저장한다
+			checked.put(car, false); 
 		}
 		
 		int result = 0;
 		int index = 0;
 		for(int i = 0; i < N; i++) {
 			String car = br.readLine();
-			while(checked.get(list[index])) {
-				index++;
+			while(checked.get(list[index])) { // 체크를 했던 차라면 다음 순서로 넘어간다
+				index++; // 다음 체크해줘야할 차를 가리키기 위해 index++한다
 			}
-			if(!list[index].equals(car)) {
+			
+			if(!list[index].equals(car)) { // 다음 순서에 와야할 차와 같지 않다면 -> 추월한것이다
 				checked.put(car, true);
 				result++;
-			} else {
+			} else { // 다음 순서에 와야할 차와 같다면 -> 올바른 순서 , 추월하지 않은 것이다
 				checked.put(car, true);
-				index++;
+				index++; // 다음 체크해줘야할 차를 가리키기 위해 index++한다
 			}
 		}
 		
